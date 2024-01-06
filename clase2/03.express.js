2994247c59f81f694f2febde5fa340ae544e8b6d
Luis Fernando Llanes Bojorquez <luisllaboj@gmail.com>
2024-01-05 18:24:38 -0700

    clase 2, creacion de una api

--- clase2/03.express.js
+++ clase2/03.express.js
@@ -9,37 +9,72 @@ app.disable('x-powered-by')
 const PORT = process.env.PORT ?? 1234
 
 // MIDDLEWARE
-// En el primer parametro puedes definir si quieres que solo aplique a ciertas rutas, ej. '/pokemon/*' >> Todas las rutas que empiecen por '/pokemon'
-app.use((req, res, next) => {
-  // Para validar que el usuario este autenticado
-  // Revisar cookies
-  console.log(`request de url ${req.url} pasando por el middleware`)
+// En el primer parametro puedes definir si quieres que solo aplique a ciertas rutas, ej. '/pokemon/*' >> Todas las rutas que empiecen por '/pokemon', el default es '/' por eso entra en todas
+// app.use((req, res, next) => {
+//   // Para validar que el usuario este autenticado
+//   // Revisar cookies
+//   // Procesar cierto tipos de datos
+//   // Rechazar requests, etc
+//   console.log(`request de url ${req.url} pasando por el middleware`)
 
-  if (req.method !== 'POST') return next()
-  if (req.headers['content-type'] !== 'application/json') return next()
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
 
-  // En este punto solo tendremos peticiones POST y el contenido sera un JSON
+//   // En este punto solo tendremos peticiones POST y el contenido sera un JSON
 
-  let body = ''
+//   let body = ''
 
-  req.on('data', (chunk) => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    data.timestamp = Date.now()
-    req.body = data
+//   req.on('data', (chunk) => {
+//     body += chunk.toString()
+//   })
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     req.body = data
 
-    next()
-  })
-})
-// express ya cuenta con un middleware que hace esto por nosotros:
-// app.use(express.json())
+//     next()
+//   })
+// })
+// >> express ya cuenta con un middleware que hace esto por nosotros:
+app.use(express.json())
 
 // >>>>>>>>>>>>>>> GET <<<<<<<<<<<<<<<
 app.get('/', (req, res) => {
-  res.send('<h1>Servidor con express</h1>')
+  res.send('<h1>Servidor con express /</h1>')
 })
+
+app.get(
+  '/apple',
+  (req, res, next) => {
+    console.log('pasando por la primera ruta, next("route")')
+    next('route')
+    // res.send('<h1>/apple</h1>')
+  },
+  (req, res, next) => {
+    console.log('pasando por la segunda ruta')
+
+    res.send('<h1>/apple 2 junto</h1>')
+  }
+)
+
+app.get('/apple', (req, res, next) => {
+  console.log('pasando por la tercera ruta')
+
+  res.send('<h1>/apple 2 separado</h1>')
+})
+
+app.get('/apple/images', (req, res) => {
+  res.send('<h1>/apple/images</h1>')
+})
+
+app.get('/apple/images/news', (req, res) => {
+  res.send('/apple/images/news</h1>')
+})
+
+app.get('/pokemon/:pokemonId', (req, res) => {
+  res.json(req.params)
+})
+
 app.get('/pokemon/ditto', (req, res) => {
   res.json(dittoJson)
 })
@@ -53,6 +88,10 @@ app.post('/pokemon', (req, res) => {
   res.status(201).json(req.body)
 })
 
+// El handler de errores toma cualquier cosa que se le mande a next(.....) que no sea next('route')
+app.use((err, req, res, next) => {
+  console.log('catching error from: ', err)
+})
 // Los metodos que no hayan hecho un match
 app.use((req, res) => {
   res.status(404).send('<h1>404</h1>')
