7ffb51480aedfceb77279d3c07e5605b86f0eb47
Luis Llanes <luisllaboj@gmail.com>
2024-06-15 00:53:52 -0700

    add morgan

--- 07-user-auth/src/index.ts
+++ 07-user-auth/src/index.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 
 dotenv.config();
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hola mundo');
