8902d2b3afcaa31f20c5ee8560386ff84edf43b3
Luis Llanes <luisllaboj@gmail.com>
2024-06-15 18:13:37 -0700

    add authRouter endpoints (no logic)

--- 07-user-auth/src/app.ts
+++ 07-user-auth/src/app.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import morgan from 'morgan';
 import { PORT } from './config';
 import { errorMiddleware, notFoundMiddleware } from './middleware/error-handler';
+import authRouter from './routes/auth';
 
 export default function createApp() {
   const app = express();
+
   app.use(morgan('dev'));
   app.use(express.json());
 
   // todo: routes
+  app.use('/auth', authRouter);
 
   app.use(notFoundMiddleware);
   app.use(errorMiddleware);
