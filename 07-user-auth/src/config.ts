69a44320492ba0844eee8d7c2a2a5123077774a2
Luis Llanes <luisllaboj@gmail.com>
2024-06-15 19:28:35 -0700

    add config

--- 07-user-auth/src/config.ts
+++ 07-user-auth/src/config.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT ?? 3000;
+export const { PORT = process.env.PORT ?? 3000, HASH_SALT_ROUNDS = 10 } = process.env;
