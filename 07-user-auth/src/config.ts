bd01963e6a9b0d9ddf07af0478e4b84cbea298ba
Luis Llanes <luisllaboj@gmail.com>
2024-06-16 16:37:12 -0700

    add config & env variables

--- 07-user-auth/src/config.ts
+++ 07-user-auth/src/config.ts
@@ -1,4 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const { PORT = process.env.PORT ?? 3000, HASH_SALT_ROUNDS = 10 } = process.env;
+export const {
+  PORT = process.env.PORT ?? 3000,
+  HASH_SALT_ROUNDS = 10,
+  DB_URL = process.env.DB_URL,
+  DB_TOKEN = process.env.DB_TOKEN,
+} = process.env;
