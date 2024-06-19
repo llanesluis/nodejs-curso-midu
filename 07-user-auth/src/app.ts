f806e02b6e0f741355588ce37835b5945f38f83e
Luis Llanes <luisllaboj@gmail.com>
2024-06-19 03:46:52 -0700

    add routes & middlewares from ext packages

--- 07-user-auth/src/app.ts
+++ 07-user-auth/src/app.ts
@@ -1,19 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config';
 import { errorMiddleware, notFoundMiddleware } from './middleware/error-handler';
 import authRouter from './routes/auth-router';
 import usersRouter from './routes/user-router';
+import protectedRouter from './routes/protected-router';
 
 export default function createApp() {
   const app = express();
 
+  app.set('view engine', 'ejs');
   app.use(morgan('dev'));
   app.use(express.json());
+  app.use(cookieParser());
 
-  // todo: routes
   app.use('/auth', authRouter);
   app.use('/users', usersRouter);
+  app.use('/protected', protectedRouter);
 
   app.use(notFoundMiddleware);
   app.use(errorMiddleware);
